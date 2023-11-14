9a53acebceb463e6a76cccd6337ddef4890f65b1
Goncalo Fontao De Carvalho Santos Pereira <gfontao-@c1r17s1.42lisboa.com>
2023-11-14 13:59:25 +0000

    push swap 1 test done

--- commoncore/push_swap/includes/checker.h
+++ commoncore/push_swap/includes/checker.h
@@ -6,7 +6,7 @@
 /*   By: gfontao- <gfontao-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 13:01:41 by gfontao-          #+#    #+#             */
-/*   Updated: 2023/11/08 18:20:04 by gfontao-         ###   ########.fr       */
+/*   Updated: 2023/11/14 13:56:39 by gfontao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "push_swap.h"
 
-void checker(t_stack *stack_a, t_stack *stack_b)
+void checker(t_stack *stack_a, t_stack *stack_b);
 t_list *get_moves(void);
 t_bool	interpreter2(t_stack *stack_a, t_stack *stack_b, char *str);
 t_bool mutual_interpreter2(t_stack *stack_a, t_stack *stack_b, char *str);
