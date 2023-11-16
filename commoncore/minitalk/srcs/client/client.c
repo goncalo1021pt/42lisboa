bdc6147bf503aaac94b429f2c62db5ecfd78a76d
Goncalo Fontao De Carvalho Santos Pereira <gfontao-@c1r17s1.42lisboa.com>
2023-11-16 17:44:54 +0000

    backup

--- commoncore/minitalk/srcs/client/client.c
+++ commoncore/minitalk/srcs/client/client.c
@@ -6,21 +6,21 @@
 /*   By: gfontao- <gfontao-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 15:49:58 by gfontao-          #+#    #+#             */
-/*   Updated: 2023/11/16 10:19:59 by gfontao-         ###   ########.fr       */
+/*   Updated: 2023/11/16 17:44:48 by gfontao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/client.h"
+#include "../../includes/minitalk.h"
 
-void exit_msg(char *msg)
+void	exit_msg(char *msg)
 {
 	ft_putstr_fd(msg, 2);
 	exit(1);
 }
 
-void check_input(char *input, int *pid)
+void	check_input(char *input, int *pid)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (input[i])
@@ -29,48 +29,53 @@ void check_input(char *input, int *pid)
 			exit_msg("Invalid PID\n");
 		i++;
 	}
-	if (ft_strlen(input) > 5)
+	if (ft_strlen(input) > 6)
 		exit_msg("Invalid PID\n");
 	*pid = ft_atoi(input);
 	if (*pid < 0)
 		exit_msg("Invalid PID\n");
-
 }
 
-void signal_handler(char c, pid_t pid)
+void	signal_handler(char c, pid_t pid)
 {
-	int ctd;
-	int error;
-	const int sleep_time = 100;
+	int			ctd;
+	int			error;
+	const int	sleep_time = 100;
 
 	ctd = 0;
+	ft_printf("PID: %d\n", pid);
 	while (ctd < 8)
 	{
 		if (c & 1)
 		{
-			error =  kill(pid, SIGUSR1);
+			ft_printf("Sending: 1\n");
+			error = kill(pid, SIGUSR1);
 			if (error == -1)
 				exit_msg("Invalid PID\n");
 		}
 		else
 		{
-			error =  kill(pid, SIGUSR2);
+			ft_printf("Sending: 0\n");
+			error = kill(pid, SIGUSR2);
+			ft_printf("Error: %d\n", error);
 			if (error == -1)
 				exit_msg("Invalid PID\n");
 		}
 		c = c >> 1;
+		ft_printf("Sleeping for %d microseconds\n", sleep_time);
 		usleep(sleep_time);
 		ctd++;
 	}
 }
 
-void send_signal(pid_t pid, char *str)
+void	send_signal(pid_t pid, char *str)
 {
-	int ctd;
+	int	ctd;
 
 	ctd = 0;
 	while (1)
 	{
+		ft_printf("Sending: %c\n", str[ctd]);
 		signal_handler(str[ctd], pid);
 		ctd++;
 		if (str[ctd] == '\0')
@@ -78,9 +83,9 @@ void send_signal(pid_t pid, char *str)
 	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	pid_t pid;
+	pid_t	pid;
 
 	if (argc != 3)
 		return (0);
--- commoncore/minitalk/srcs/server/server.c
+++ commoncore/minitalk/srcs/server/server.c
@@ -6,23 +6,51 @@
 /*   By: gfontao- <gfontao-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 15:45:18 by gfontao-          #+#    #+#             */
-/*   Updated: 2023/11/16 10:24:32 by gfontao-         ###   ########.fr       */
+/*   Updated: 2023/11/16 13:49:03 by gfontao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minitalk.h"
 
-void recive_message()
-{
+#define MESSAGE_SIZE 1000
 
+typedef struct s_message
+{
+	char buffer[MESSAGE_SIZE];
+	int index;
+} t_message;
+
+void handle_signal(int signal, siginfo_t *info, void *context)
+{
+	static t_message message = {{0}, 0};
+
+	(void)context;
+	if (signal == SIGUSR1)
+		message.buffer[message.index++] = '0';
+	else if (signal == SIGUSR2)
+		message.buffer[message.index++] = '1';
+	if (message.index == 8 * sizeof(message.buffer))
+	{
+		message.buffer[message.index] = '\0';
+		ft_printf("%s\n", message.buffer);
+		message.index = 0;
+		ft_memset(message.buffer, 0, sizeof(message.buffer));
+	}
+	kill(info->si_pid, SIGUSR1);
 }
 
-int main()
+int main(void)
 {
 	pid_t pid;
+	struct sigaction action = {0};
 
 	pid = getpid();
 	ft_printf("PID: %d\n", pid);
-
+	action.sa_sigaction = handle_signal;
+	action.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &action, NULL);
+	sigaction(SIGUSR2, &action, NULL);
+	while (1)
+		pause();
 	return (0);
 }
